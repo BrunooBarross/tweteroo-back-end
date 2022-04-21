d5ca946127bb60e025439c92fca249d23c1aefc1
Bruno Barros <barrosbrunofelipe@gmail.com>
2022-04-21 18:22:43 -0300

    feat: Adicionando função post para receber os tweets do usuários logado

--- src/app.js
+++ src/app.js
@@ -8,10 +8,26 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const dadosUsuario = [];
+const tweets = [
+	{
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	    tweet: "eu amo o hub"
+	}
+]
 
 app.post("/sign-up", (req, res) => {
-    dadosUsuario.push({nome: req.body.username, foto: req.body.avatar} )
+    dadosUsuario.push({nome: req.body.username, avatar: req.body.avatar} )
     res.send(console.log(dadosUsuario));
+});
+
+app.get("/tweets", (req, res) => {
+    res.send(tweets);
+});
+
+app.post("/tweets", (req, res) => {
+    tweets.push({nome: req.body.username, avatar: dadosUsuario[0].avatar, tweet: req.body.tweet} )
+    res.send(console.log(dadosUsuario[0].avatar));
 });
 
 app.listen(5000, console.log("Server rodando na port 5000"));
