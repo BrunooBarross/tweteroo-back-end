9bbd0811efff6f2e4dea86678b16d8b8f11a882f
Bruno Barros <barrosbrunofelipe@gmail.com>
2022-04-21 16:34:41 -0300

    feat: Criando função post e recebendo dados do usuário via bodyParser

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send("funcionando");
+const dadosUsuario = [];
+
+app.post("/sign-up", (req, res) => {
+    dadosUsuario.push({nome: req.body.username, foto: req.body.avatar} )
+    res.send(console.log(dadosUsuario));
 });
 
-app.listen(5000);
+app.listen(5000, console.log("Server rodando na port 5000"));
