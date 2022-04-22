1072e5a69bea737aa191caa370bc791a250e1611
Bruno Barros <barrosbrunofelipe@gmail.com>
2022-04-22 10:29:52 -0300

    feat: removendo pacote npm do body-paser, refatorando projeto para utilizar o json e req.body do próprio express

--- src/app.js
+++ src/app.js
@@ -1,33 +1,32 @@
 import express from 'express';
 import cors from 'cors';
-import bodyParser from 'body-parser';
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cors());
 
 const dadosUsuario = [];
 const tweets = [
-	{
-		username: "bobesponja",
-		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-	    tweet: "eu amo o hub"
-	}
+    {
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "eu amo o hub"
+    }
 ]
 
 app.post("/sign-up", (req, res) => {
-    dadosUsuario.push({nome: req.body.username, avatar: req.body.avatar} )
+    dadosUsuario.push({ nome: req.body.username, avatar: req.body.avatar })
     res.send("OK");
 });
 
 app.get("/tweets", (req, res) => {
-    res.send(tweets.slice(0,10));
+    res.send(tweets.slice(0, 10));
 });
 
 app.post("/tweets", (req, res) => {
     let avatarUsuario = dadosUsuario.find(element => element.nome === req.body.username);
-    tweets.unshift({username: req.body.username, avatar: avatarUsuario.avatar, tweet: req.body.tweet} )
+    tweets.unshift({ username: req.body.username, avatar: avatarUsuario.avatar, tweet: req.body.tweet })
     res.send("OK");
 });
 
